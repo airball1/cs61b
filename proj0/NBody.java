90fb1ea562ccf0d0729cf358552d8aafada5bb27
airball1 <lzkx1234@gmail.com>
2018-02-03 23:10:59 +0800

    completed proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -27,13 +27,39 @@ public class NBody {
 		String filename = new String();
 		filename = args[2];
 		Planet[] allPlanets = NBody.readPlanets(filename);
+		
 		double radius = NBody.readRadius(filename);
 		StdDraw.setScale(-radius, radius);
-		StdDraw.picture(0, 0, "images/starfield.jpg");
-		for (int i  = 0; i < allPlanets.length; i++) {
-			allPlanets[i].draw();
+		
+		StdDraw.enableDoubleBuffering();
+		
+		
+		double time = 0.0;
+		double[] xForces = new double[allPlanets.length];
+		double[] yForces = new double[allPlanets.length];
+		while (time < T) {
+			for (int i = 0; i < allPlanets.length; i++) {
+				xForces[i] = allPlanets[i].calcNetForceExertedByX(allPlanets);
+				yForces[i] = allPlanets[i].calcNetForceExertedByX(allPlanets);		
+			}
+			
+			for (int i = 0; i < allPlanets.length; i++) {
+				allPlanets[i].update(dt, xForces[i], yForces[i]);
+			}
+			
+			StdDraw.picture(0, 0, "images/starfield.jpg");
+			
+			for (int i = 0; i < allPlanets.length; i++) {
+				allPlanets[i].draw();
+			}
+			StdDraw.show();
+			StdDraw.pause(10);
+			time = time + dt;
 		}
+		StdOut.printf("%d\n", allPlanets.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < allPlanets.length; i++) {
+			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n", allPlanets[i].xxPos, allPlanets[i].yyPos, allPlanets[i].xxVel, allPlanets[i].yyVel, allPlanets[i].mass, allPlanets[i].imgFileName);   
+        }
 	}
-	
-	
 }
